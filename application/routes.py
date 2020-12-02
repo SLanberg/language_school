0dcc827fd75a2fb40cfb0400248616b516852a4c
SLanberg <stefan.lanberg@gmail.com>
2020-12-02 11:56:14 +0200

    new_url_routes_and_html_templates

--- application/routes.py
+++ application/routes.py
@@ -6,3 +6,13 @@ from flask import render_template
 @app.route('/index')
 def home():
     return render_template("home.html")
+
+
+@app.route('/prices')
+def prices():
+    return render_template("prices.html")
+
+
+@app.route('/teachers')
+def teachers():
+    return render_template("teachers.html")
